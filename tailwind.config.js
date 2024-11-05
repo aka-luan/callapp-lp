06689fb9569550af34ac892649048ac9d8be993a
Luan Alves <akaluan.la@gmail.com>
2024-11-05 00:20:10 -0300

    sum tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,29 @@
+const plugin = require('tailwindcss');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        green: 'var(--green)',
+        lightgreen: 'var(--lightgreen)',
+        purple: 'var(--purple)',
+      },
+      fontFamily: {
+        sans: ['Lexend', 'sans-serif'],
       },
     },
   },
