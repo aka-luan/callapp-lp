fa972a2e82bb87dc0d5257506c855398325a7975
Luan Alves <akaluan.la@gmail.com>
2024-11-06 00:08:40 -0300

    btn default prop * adjusted padding y

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -1,8 +1,8 @@
-export default function Button({ isNav, type, children }) {
+export default function Button({ isNav = false, type, children }) {
   return (
     <button
       className={`hover:scale-90 px-7 ${
-        isNav ? 'py-[10px]' : 'py-5'
+        isNav ? 'py-[10px]' : 'py-3'
       } border-lightgreen border-2 ${
         type === 'primary'
           ? 'bg-lightgreen text-green'
