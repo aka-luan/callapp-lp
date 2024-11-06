0629cead38759106be9df9cee0ed05b07dfdfcb2
Luan Alves <akaluan.la@gmail.com>
2024-11-06 00:13:45 -0300

    some of the hero text (need to adjust margins)

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,25 @@
-import Header from "./components/Header";
+import Button from './components/Button';
+import Header from './components/Header';
 
 export default function Home() {
   return (
-    <div className="">
+    <div className=''>
       <Header />
-      <h1>hello world</h1>
+      <main>
+        <section id='hero' className='bg-green py-44 text-lightgreen'>
+          <div className='container flex mx-auto'>
+            <div id='hero-text' className='flex flex-col gap-10'>
+              <span className='h-[2px] w-16 bg-lightgreen opacity-60'></span>
+              <h1>Have your best chat</h1>
+              <p>Fast, easy & unlimited team chats.</p>
+              <div className='flex'>
+                <Button type={'primary'}>Try it Free</Button>
+                <Button>Get a Demo</Button>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
