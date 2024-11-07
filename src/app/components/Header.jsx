31902eaa4cacaa0e6bd413b73ff0286ee6a56b85
Luan Alves <akaluan.la@gmail.com>
2024-11-07 00:03:21 -0300

    chaging padding container width

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -4,7 +4,7 @@ import Button from './Button';
 export default function Header() {
   return (
     <header className='bg-green text-lightgreen text-sm'>
-      <div className='flex container mx-auto justify-between'>
+      <div className='flex px-12 justify-between'>
         <a href='#'>
           <Image
             src='/logo.svg'
