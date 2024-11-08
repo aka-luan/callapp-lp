1d58d9bf661e31e2d3c1d1591be74b5ee3c9efe7
Luan Alves <akaluan.la@gmail.com>
2024-11-07 23:41:30 -0300

    cta section done

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import Button from './components/Button';
 import Header from './components/Header';
-import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -13,7 +12,8 @@ export default function Home() {
             <div id='hero-text' className='flex flex-col flex-1'>
               <span className='h-[2px] w-16 bg-lightgreen opacity-60 mb-7'></span>
               <h1 className='mb-7 font-medium'>
-                Have your<br></br> best <span className='text-purple'>chat</span>
+                Have your<br></br> best{' '}
+                <span className='text-purple'>chat</span>
               </h1>
               <p className='mb-14 font-light'>
                 Fast, easy & unlimited team chats.
@@ -23,8 +23,8 @@ export default function Home() {
                 <Button type={'secondary1'}>Get a Demo</Button>
               </div>
             </div>
-            <div id="hero-img flex-1">
-              <Image src='/Hero Images.png' width={600} height={600}></Image>
+            <div id='hero-img flex-1'>
+              <Image src='/Hero Images.png' width={600} height={600} />
             </div>
           </div>
         </section>
@@ -39,6 +39,7 @@ export default function Home() {
               Donec quam felis, ultricies nec,
             </p>
             <Image
+              className='ml-[44px]'
               src='/middle-section-img.png'
               width={1204}
               height={917}
@@ -46,22 +47,39 @@ export default function Home() {
             />
           </div>
         </section>
-        <section id='cta' className='py-40 px-12 bg-lightgreen text-foreground flex'>
+        <section
+          id='cta'
+          className='py-40 px-12 bg-lightgreen text-foreground flex gap-24'>
           <Image
             src='/cta-img.png'
             width={705}
             height={734}
             alt='two mans talking in an modern office with some people in the background working and talking'
           />
-          <div className='flex container flex-col justify-center'>
-            <h2 className='mb-5 font-medium'>Perfect Solution for <br></br>Small Businesses</h2>
-            <p className='mb-14 font-light w-1/2 '>
+          <div className='flex flex-col justify-center w-[450px]'>
+            <h2 className='mb-5 font-medium '>
+              Perfect Solution for Small Businesses
+            </h2>
+            <p className='mb-10 font-light'>
               Pricing plans that fit like a glove.
             </p>
-            <div className='flex gap-7'>
-                <Button type={'primary2'}>Try it Free</Button>
-                <Button type={'secondary2'}>Get a Demo</Button>
-              </div>
+            <div className='mb-12 flex gap-7'>
+              <Button type={'primary2'}>Try it Free</Button>
+              <Button type={'secondary2'}>Get a Demo</Button>
+            </div>
+            <span className='h-[2px] w-12 bg-foreground opacity-60 mb-5'></span>
+            <div className='flex gap-4 items-center'>
+              <Image
+                src='/cta-stars.png'
+                width={147}
+                height={28}
+                alt='five green stars'
+              />
+              <small>
+                <b>5,200 businesses</b> use ChatApp and they rate it as{' '}
+                <b>5-stars.</b>
+              </small>
+            </div>
           </div>
         </section>
       </main>
