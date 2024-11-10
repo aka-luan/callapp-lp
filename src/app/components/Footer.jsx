eb5ff30b0c37cbe58848511261fa846facad7da2
Luan Alves <akaluan.la@gmail.com>
2024-11-09 22:07:23 -0300

    footer ok, only missing socials

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,9 +1,8 @@
 import Image from 'next/image';
-
 export default function Footer() {
   return (
-    <footer id='footer' className='pt-40 pb-5'>
-      <div className='container mx-auto grid grid-cols-[2fr_1fr_1fr_1fr_1fr]'>
+    <footer id='footer' className='container mx-auto pt-40 pb-5'>
+      <div className='grid grid-cols-[2fr_1fr_1fr_1fr_1fr] mb-32  '>
         <div className='flex flex-col'>
           <Image
             className='mb-7'
@@ -12,114 +11,202 @@ export default function Footer() {
             height={105}
             alt='chatapp writen in light green'
           />
-          <small className='opacity-80 mb-72'>
+          <small className='opacity-60'>
             The last team chat you’ll ever need.
           </small>
-          <small className='opacity-80'>© Copyright ChatApp Inc.</small>
         </div>
         <div className='flex flex-col'>
           <h3 className='mb-7'>Help</h3>
-          <ul className='flex flex-col gap-6'>
-            <li className='opacity-80'>
-              <small><a
-                className='hover:border-b-2 border-foreground pb-1'
-                href='#footer'>
-                Support
-              </a></small>
+          <ul className='flex flex-col gap-4'>
+            <li className='opacity-60'>
+              <small>
+                <a
+                  className='hover:border-b-2 border-foreground pb-1'
+                  href='#footer'>
+                  Support
+                </a>
+              </small>
             </li>
-            <li className='opacity-80'>
-              <small><a
-                className='hover:border-b-2 border-foreground pb-1'
-                href='#footer'>
-                Knowledgebase
-              </a></small>
+            <li className='opacity-60'>
+              <small>
+                <a
+                  className='hover:border-b-2 border-foreground pb-1'
+                  href='#footer'>
+                  Knowledgebase
+                </a>
+              </small>
             </li>
-            <li className='opacity-80'>
-              <small><a
-                className='hover:border-b-2 border-foreground pb-1 '
-                href='#footer'>
-                Tutorials
-              </a></small>
+            <li className='opacity-60'>
+              <small>
+                <a
+                  className='hover:border-b-2 border-foreground pb-1 '
+                  href='#footer'>
+                  Tutorials
+                </a>
+              </small>
             </li>
           </ul>
         </div>
         <div className='flex flex-col'>
-          <h3 className='mb-7'>Help</h3>
-          <ul className='flex flex-col gap-6'>
-            <li className='opacity-80'>
-              <small><a
-                className='hover:border-b-2 border-foreground pb-1'
-                href='#footer'>
-                Support
-              </a></small>
+          <h3 className='mb-7'>Features</h3>
+          <ul className='flex flex-col gap-4'>
+            <li className='opacity-60'>
+              <small>
+                <a
+                  className='hover:border-b-2 border-foreground pb-1'
+                  href='#footer'>
+                  Screen Sharing
+                </a>
+              </small>
             </li>
-            <li className='opacity-80'>
-              <small><a
-                className='hover:border-b-2 border-foreground pb-1'
-                href='#footer'>
-                Knowledgebase
-              </a></small>
+            <li className='opacity-60'>
+              <small>
+                <a
+                  className='hover:border-b-2 border-foreground pb-1'
+                  href='#footer'>
+                  iOS & Android Apps
+                </a>
+              </small>
             </li>
-            <li className='opacity-80'>
-              <small><a
-                className='hover:border-b-2 border-foreground pb-1 '
-                href='#footer'>
-                Tutorials
-              </a></small>
+            <li className='opacity-60'>
+              <small>
+                <a
+                  className='hover:border-b-2 border-foreground pb-1 '
+                  href='#footer'>
+                  File Sharing
+                </a>
+              </small>
             </li>
-          </ul>
-        </div>
-        <div className='flex flex-col'>
-          <h3 className='mb-7'>Help</h3>
-          <ul className='flex flex-col gap-6'>
-            <li className='opacity-80'>
-              <small><a
-                className='hover:border-b-2 border-foreground pb-1'
-                href='#footer'>
-                Support
-              </a></small>
-            </li>
-            <li className='opacity-80'>
-              <small><a
-                className='hover:border-b-2 border-foreground pb-1'
-                href='#footer'>
-                Knowledgebase
-              </a></small>
-            </li>
-            <li className='opacity-80'>
-              <small><a
-                className='hover:border-b-2 border-foreground pb-1 '
-                href='#footer'>
-                Tutorials
-              </a></small>
+            <li className='opacity-60'>
+              <small>
+                <a
+                  className='hover:border-b-2 border-foreground pb-1 '
+                  href='#footer'>
+                  User Management
+                </a>
+              </small>
             </li>
           </ul>
         </div>
         <div className='flex flex-col'>
-          <h3 className='mb-7'>Help</h3>
-          <ul className='flex flex-col gap-6'>
-            <li className='opacity-80'>
-              <small><a
-                className='hover:border-b-2 border-foreground pb-1'
-                href='#footer'>
-                Support
-              </a></small>
+          <h3 className='mb-7'>Company</h3>
+          <ul className='flex flex-col gap-4'>
+            <li className='opacity-60'>
+              <small>
+                <a
+                  className='hover:border-b-2 border-foreground pb-1'
+                  href='#footer'>
+                  About Us
+                </a>
+              </small>
             </li>
-            <li className='opacity-80'>
-              <small><a
-                className='hover:border-b-2 border-foreground pb-1'
-                href='#footer'>
-                Knowledgebase
-              </a></small>
+            <li className='opacity-60'>
+              <small>
+                <a
+                  className='hover:border-b-2 border-foreground pb-1'
+                  href='#footer'>
+                  Careers
+                </a>
+              </small>
             </li>
-            <li className='opacity-80'>
-              <small><a
-                className='hover:border-b-2 border-foreground pb-1 '
-                href='#footer'>
-                Tutorials
-              </a></small>
+            <li className='opacity-60'>
+              <small>
+                <a
+                  className='hover:border-b-2 border-foreground pb-1 '
+                  href='#footer'>
+                  Contact Us
+                </a>
+              </small>
             </li>
           </ul>
+        </div>
+        <div className='flex flex-col'>
+          <h3 className='mb-7'>Contact Us</h3>
+          <ul className='flex flex-col gap-4'>
+            <li className='opacity-60'>
+               <small>
+                <a
+                  className='hover:border-b-2 border-foreground pb-1'
+                  href='mailto:info@chatapp.com'>
+                  info@chatapp.com
+                </a>
+              </small>
+            </li>
+            <li className='opacity-60'>
+              <small>
+                <a
+                  className='hover:border-b-2 border-foreground pb-1'
+                  href='#footer'>
+                  1-800-200-300
+                </a>
+              </small>
+            </li>
+            <li className='opacity-60'>
+              <small>
+                <a
+                  className='hover:border-b-2 border-foreground pb-1 '
+                  href='https://maps.app.goo.gl/3yfF3t44uskiZUsU9'
+                  target='_blank'>
+                  3500 Deer Creek Rd Palo A
+                </a>
+              </small>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className='grid grid-cols-[2fr_3fr_1fr]'>
+        <small className='opacity-60'>© Copyright ChatApp Inc.</small>
+        <ul className='flex gap-4'>
+          <li className='opacity-60'>
+            <small>
+              <a
+                className='hover:border-b-2 border-foreground pb-1'
+                href='#footer'>
+                Terms of Service
+              </a>
+            </small>
+          </li>
+          <li className='opacity-60'>
+            <small>
+              <a
+                className='hover:border-b-2 border-foreground pb-1'
+                href='#footer'>
+                Privacy Policy
+              </a>
+            </small>
+          </li>
+          <li className='opacity-60'>
+            <small>
+              <a
+                className='hover:border-b-2 border-foreground pb-1 '
+                href='#footer'>
+                Partners & Affiliates
+              </a>
+            </small>
+          </li>
+          <li className='opacity-60'>
+            <small>
+              <a
+                className='hover:border-b-2 border-foreground pb-1 '
+                href='#footer'>
+                Sitemap
+              </a>
+            </small>
+          </li>
+        </ul>
+        <div className='flex items-center gap-4'>
+          <a href='https://x.com' target='_blank'>
+            
+          </a>
+          <a  href='https://facebook.com' target='_blank'>
+            
+          </a>
+          <a  href='https://linkedin.com' target='_blank'>
+            
+          </a>
+          <a  href='https://instagram.com' target='_blank'>
+            
+          </a>
         </div>
       </div>
     </footer>
